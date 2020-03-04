5d9b661956dfc1fdd7f543d00133dfef777f3017
Rupture13 <marco.nieuwenhuizen@student.fontys.nl>
2020-03-04 11:07:13 +0100

    Basic Project Setup
    Capsule-Chaos

---
+++ Assets/Editor/BasicTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicTests : MonoBehaviour
+{
+    [Test]
+    public void SomeGreenTestPassing()
+    {
+        Assert.Pass();
+    }
+}
