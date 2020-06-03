565fd3aa7169bc57eda913318360dc59141a6cdd
Rupture13 <marco.nieuwenhuizen@student.fontys.nl>
2020-06-03 20:22:08 +0200

    Added Main Menu
    And generated some lightmaps

---
+++ CapsuleChaos_UnityGame/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    private int currentLevelId = 0;
+
+    private void Start()
+    {
+        Physics.gravity = new Vector3(0, -9.81f, 0);
+    }
+
+    public void SetLevelId(int levelId)
+    {
+        currentLevelId = levelId;
+    }
+
+    public void StartLevel()
+    {
+        SceneManager.LoadScene(currentLevelId);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
