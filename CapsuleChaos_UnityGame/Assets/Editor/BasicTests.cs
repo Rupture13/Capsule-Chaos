f701d96322c48f3c015a7e9e88477d728f76fbc3
Rupture13 <marco.nieuwenhuizen@student.fontys.nl>
2020-06-21 16:12:31 +0200

    Added proper unit tests for testing in CI pipeline

--- CapsuleChaos_UnityGame/Assets/Editor/BasicTests.cs
+++ CapsuleChaos_UnityGame/Assets/Editor/BasicTests.cs
@@ -1,13 +1,46 @@
 ﻿using NUnit.Framework;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BasicTests : MonoBehaviour
 {
-    [Test]
-    public void SomeGreenTestPassing()
+    private GameObject go;
+    private PlayerScore playerScore;
+
+    [SetUp]
+    public void SetUp()
     {
-        Assert.Pass();
+        go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        playerScore = go.AddComponent<PlayerScore>();
+        playerScore.score = 0;      //0 score
+        playerScore.timer = 20.00f; //20 seconds
+    }
+
+    [Test]
+    public void TestPlayerScoreScoreAdd()
+    {
+        try
+        {
+            playerScore.AddScore(1);
+        }
+        catch (System.NullReferenceException e)
+        {
+            Debug.Log($"Expected NullReference in testing: '{e.Message}'");
+        }
+        
+        Assert.AreEqual(1, go.GetComponent<PlayerScore>().score);
+    }
+
+    [Test]
+    public void TestPlayerScoreTimer()
+    {
+        Assert.AreEqual(20.00f, go.GetComponent<PlayerScore>().timer);
+        Assert.AreEqual(2000, playerScore.GetTimeInteger());
+    }
+
+    [Test]
+    public void TestFormatTimeUtility()
+    {
+        var expectedTimeFormat = "00:20:00";
+        Assert.AreEqual(expectedTimeFormat, Utils.FormatTime(playerScore.timer));
     }
 }
